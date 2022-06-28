ee4437217856d08ded94811ddb603b72c009542b
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-06-27 19:22:40 -0500

    Added static available ID variable in doctor class

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,6 +1,18 @@
 public class Doctor {
+    static int nextValidId = 0;
     int id;
     String name;
+
+    Doctor() {
+        id = ++nextValidId;
+    }
+
+    Doctor(String name) {
+        this.name = name;
+        id = ++nextValidId;
+        System.out.println("The assigned doctor's name is: " + this.name);
+        System.out.println(id);
+    }
 
     public void printName() {
         System.out.println(name);
