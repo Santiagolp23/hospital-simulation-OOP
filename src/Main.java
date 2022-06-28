4cd8f06d59cbcebbbc489743199283cd18033a9b
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-06-27 19:37:08 -0500

    Made the import static on main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import ui.UIMenu;
+import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor("Diego Savarra");
         Doctor myDoctorSantiago = new Doctor();
         Doctor.showNextValidId();
-        UIMenu.showMenu();
+        showMenu();
     }
 }
